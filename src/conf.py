8387fde82554f96f0e5bf99a0a7a5c7f3fb10010
Serhii Horodilov <sgorodil@gmail.com>
2023-12-23 18:19:33 +0200

    Adjust PDF docs list

--- src/conf.py
+++ src/conf.py
@@ -88,11 +88,7 @@ latex_doc = project.title().replace(" ", "") + ".tex"
 latex_engine = "xelatex"
 latex_documents = [
     (master_doc, latex_doc, project, authors, "manual", False),
-    ("appx/software_list", "Software.tex", project, authors, "howto", False),
-    ("appx/code_edit", "IDE.tex", project, authors, "howto", False),
-    ("appx/blog/spec", "DjangoBlog.tex", project, authors, "howto", False),
-    ("appx/assignments", "CreatingPullRequest.tex",
-     "Creating the Pull Request", authors, "howto", False),
+    # TODO: set up additional documents to build separately
 ]
 latex_appendices = [
     # TODO: review appendices list
